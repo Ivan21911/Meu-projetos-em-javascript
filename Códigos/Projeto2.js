84068243e632f82f6bdba41244a8d5965b971f2e
Ivan log 2 <93832683+Ivan21911@users.noreply.github.com>
2023-08-03 18:16:31 -0300

    Criado projeto2.js

---
+++ Códigos/Projeto2.js
@@ -0,0 +1,24 @@
+//Um programa que reconhece um número positivo/negativo!
+const number = prompt("Digite um Número");
+
+if (number > 0) {
+  console.log("O número é positivo");
+ } 
+
+else if (number < 0) {
+  console.log("O número é negativo");
+}
+
+/*
+
+    |-------    °          /\        /\
+    |           |         /  \      /  \
+    |-------    |        /    \    /    \
+    |           |       /      \  /      \
+    |           |      /        \/        \
+    |           |     /                    \
+  
+ 
+    
+
+*/   
